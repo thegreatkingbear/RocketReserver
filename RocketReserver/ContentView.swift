59ad0e84c69da78b6539c9225f204dcf9e364e04
Mookyung <pporri@yonsei.ac.kr>
2020-06-12 10:23:35 +0900

    Initial Commit

---
+++ RocketReserver/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RocketReserver
+//
+//  Created by Mookyung Kwak on 2020/06/12.
+//  Copyright © 2020 Mookyung Kwak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
